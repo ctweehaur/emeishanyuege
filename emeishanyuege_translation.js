386a842c8290ceb14548100f1c7e06f62025eeba
ctweehaur <ctweehaur@gmail.com>
2026-05-29 13:10:59 +0800

    Update emeishanyuege_translation.js

--- emeishanyuege_translation.js
+++ emeishanyuege_translation.js
@@ -6,13 +6,13 @@ const translationData = [
     {
         // 第一、二句翻译
         zh: "峨眉山 上空 悬挂 着 半轮 初秋 的 明月 ， 月影 倒映 在 平羌江 中 ， 随着 江水 流动 。",
-        py: "é méi shān shàng kōng xuán guà zhe bàn lún chū qiū de míng yuè ， yuè yǐng dào yìng zài píng qiāng jiāng zhōng ， suí zhe jiāng shuǐ liú dòng",
+        py: "é méi shān shàng kōng xuán guà zhe bàn lún chū qiū de míng yuè yuè yǐng dào yìng zài píng qiāng jiāng zhōng suí zhe jiāng shuǐ liú dòng",
         en: "Above Mount Emei hangs a half-round early autumn moon; its reflection is cast upon the Pingqiang River, flowing along with the water."
     },
     {
         // 第三、四句翻译
         zh: "连夜 从 清溪 驿 出发 前往 长江 三峡 ， 一路上 思念 着 你 却 不得 相见 ， 船只 已 顺流 直下 驶向 渝州 。",
-        py: "lián yè cóng qīng xī yì chū fā qián wǎng cháng jiāng sān xiá ， yí lù shàng sī niàn zhe nǐ què bù dé xiāng jiàn ， chuán zhī yǐ shùn liú zhí xià shǐ xiàng yú zhōu",
+        py: "lián yè cóng qīng xī yì chū fā qián wǎng cháng jiāng sān xiá yí lù shàng sī niàn zhe nǐ què bù dé xiāng jiàn chuán zhī yǐ shùn liú zhí xià shǐ xiàng yú zhōu",
         en: "Setting off at night from Qingxi towards the Three Gorges, I miss you but cannot see you as the boat sails downstream towards Yuzhou."
     }
 ];
